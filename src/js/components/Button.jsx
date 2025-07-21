8de763b96e4a91986dedd66b0c1331e8635252fb
William Vargas <danloveper@gmail.com>
2025-07-21 03:31:01 +0000

    Fix/ center text and buttons

--- src/js/components/Button.jsx
+++ src/js/components/Button.jsx
@@ -1,10 +1,11 @@
-
-function Button(props){
-    return (
-        <a href="#" className="btn btn-primary">
-          {props.buttonLabel}
-        </a>
-    );
+function Button(props) {
+  return (
+    <div className={`mt-3 d-flex ${props.buttonDirection}`}>
+      <a href="#" className="btn btn-primary">
+        {props.buttonLabel}
+      </a>
+    </div>
+  );
 }
 
 export default Button;
--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -5,11 +5,11 @@ function Card(props) {
     <div className="card mt-4" style={{ width: "100%" }}>
       <img src={props.imageUrl} className="card-img-top" style={{objectFit: "cover", height:"325px"}} alt={props.imageLabel} />
       <div className="card-body">
-        <h5 className="card-title">{props.title}</h5>
-        <p className="card-text">
+        <h5 className="card-title text-center">{props.title}</h5>
+        <p className="card-text text-center">
           {props.description}
         </p>
-        <Button buttonLabel={props.buttonLabel} />
+        <Button buttonLabel={props.buttonLabel} buttonDirection="justify-content-center" />
       </div>
     </div>
   );
